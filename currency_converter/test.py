bf604028083d685a96b2af54ca189f14d27dc3d3
Lahari0621 <sailaharithalanki@gmail.com>
2025-09-30 19:45:55 +0530

    Add only aicalculator project

--- currency_converter/test.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = "https://api.exchangerate.host/symbols"
-
-response = requests.get(url)
-print(response.url)  # This will show the real URL it's hitting
-print(response.json())
-
--- floyds.py
+++
@@ -1,9 +0,0 @@
-a=int(input('enter the value:'))
-k=1
-for i in range(1,a+1): 
-  for s in range(0,a-i):
-      print('',end='')
-  for j in range (1,i):
-      print('{0}'.format(k),end=' ')
-      k=k+1
-  print('\n')
